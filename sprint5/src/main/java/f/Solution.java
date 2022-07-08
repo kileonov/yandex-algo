2b6ad86e5a77762efb13b0f21550694ea9049cd8
kileonov <kileonov@ozon.ru>
2022-07-08 16:05:39 +0300

    [Sprint-5] implement max tree depth alg

---
+++ sprint5/src/main/java/f/Solution.java
@@ -0,0 +1,44 @@
+package f;
+
+public class Solution {
+    public static int treeSolution(Node head) {
+        if (head == null) return 0;
+
+        int left = treeSolution(head.left);
+        int right = treeSolution(head.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static void test() {
+        Node node1 = new Node(1, null, null);
+        Node node2 = new Node(4, null, null);
+        Node node3 = new Node(3, node1, node2);
+        Node node4 = new Node(8, null, null);
+        Node node5 = new Node(5, node3, node4);
+        int result = treeSolution(node5);
+        System.out.println("result = " + result);
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
